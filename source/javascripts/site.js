697af0b5215f26a209cee912f26fb49b28238a73
Rigault Rémy <r.rigault@me.com>
2018-10-13 18:51:43 +0200

    add nav-sticky in the left

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,24 @@
 // This is where it all goes :)
 
-//= require boostrap/dist/js/bootstrap.min.js
+//= require boostrap/dist/js/bootstrap.js
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+
+		// Show/hide menu on scroll
+		//if (scrollDistance >= 850) {
+		//		$('nav').fadeIn("fast");
+		//} else {
+		//		$('nav').fadeOut("fast");
+		//}
+
+		// Assign active class to nav links while scolling
+		$('section').each(function(i) {
+				if ($(this).position().top <= scrollDistance) {
+						$('.nav-sticky ul li a.active').removeClass('active');
+						$('.nav-sticky ul li a').eq(i).addClass('active');
+				}
+		});
+}).scroll();
