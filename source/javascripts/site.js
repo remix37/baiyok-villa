b63df325fce36ebe1d5b188873ad02bfdf8b6332
Rigault Rémy <r.rigault@me.com>
2018-10-13 18:55:38 +0200

    add libs in local

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,9 @@
 // This is where it all goes :)
 
+//= require jquery-3.3.1.slim.min/index.js
+//= require popper.min/index.js
 //= require boostrap/dist/js/bootstrap.js
+//= require smooth-scroll.polyfills.min/index.js
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
