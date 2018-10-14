3489c17efd1a6dbce86facf73cbe08338731b9a2
Rigault Rémy <r.rigault@me.com>
2018-10-15 01:58:04 +0200

    new configuration cms

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,20 +4,12 @@
 //= require popper.min/index.js
 //= require boostrap/dist/js/bootstrap.js
 //= require smooth-scroll.polyfills.min/index.js
+//= require gsap/src/minified/TweenMax.min.js
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
 $(window).scroll(function() {
 		var scrollDistance = $(window).scrollTop();
-
-		// Show/hide menu on scroll
-		//if (scrollDistance >= 850) {
-		//		$('nav').fadeIn("fast");
-		//} else {
-		//		$('nav').fadeOut("fast");
-		//}
-
-		// Assign active class to nav links while scolling
 		$('section').each(function(i) {
 				if ($(this).position().top <= scrollDistance) {
 						$('.nav-sticky ul li a.active').removeClass('active');
@@ -25,3 +17,37 @@ $(window).scroll(function() {
 				}
 		});
 }).scroll();
+
+// Animation TwinMax
+
+function animation(){
+
+	var tl = new TimelineLite();
+
+	tl.from($('.villa'), 2, {
+		width: 0,
+		ease:Power4.easeInOut
+	}, '.villa');
+
+	tl.from($('.big-title'), 1, {
+		width: 0,
+		padding: 0,
+		ease:Power4.easeInOut
+	});
+
+	tl.from($('.big-title h1'), 1, {
+		autoAlpha: 0
+	});
+
+	tl.staggerFrom($('.icon-white'), 1, {
+		autoAlpha: 0
+	}, 0.5);
+
+
+
+}
+
+
+$(document).ready(function(){
+	animation();
+});
