b2f0b7260ada86c19ac6e9b38b54bf544998c1f1
Rigault Rémy <r.rigault@me.com>
2018-10-15 11:32:42 +0200

    add anchor & finalisation animation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,20 +4,12 @@
 //= require popper.min/index.js
 //= require boostrap/dist/js/bootstrap.js
 //= require smooth-scroll.polyfills.min/index.js
+//= require gsap/src/minified/TweenMax.min.js
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
 $(window).scroll(function() {
 		var scrollDistance = $(window).scrollTop();
-
-		// Show/hide menu on scroll
-		//if (scrollDistance >= 850) {
-		//		$('nav').fadeIn("fast");
-		//} else {
-		//		$('nav').fadeOut("fast");
-		//}
-
-		// Assign active class to nav links while scolling
 		$('section').each(function(i) {
 				if ($(this).position().top <= scrollDistance) {
 						$('.nav-sticky ul li a.active').removeClass('active');
@@ -25,3 +17,55 @@ $(window).scroll(function() {
 				}
 		});
 }).scroll();
+
+// Animation TwinMax
+
+function animation(){
+
+	var tl = new TimelineLite();
+
+	tl.from($('.navbar'), 1, {
+		top: '-90px',
+		ease:Power4.easeInOut
+	});
+
+	tl.from($('.nav-sticky'), 1, {
+		autoAlpha: 0
+	});
+
+	tl.from($('.villa'), 1.5, {
+		width: 0,
+		ease:Power4.easeInOut
+	}, "-=0.8");
+
+	tl.from($('.big-title'), 1, {
+		width: 0,
+		padding: 0,
+		ease:Power4.easeInOut
+	}, "-=0.8");
+
+	tl.from($('.big-title h1'), 1, {
+		autoAlpha: 0
+	});
+
+	tl.staggerFrom($('.icon-white'), 1, {
+		autoAlpha: 0
+	}, 0.2, "-=0.5");
+
+	tl.from($('.discover'), 1, {
+		transform: 'scaleY(0)',
+		ease:Power4.easeInOut
+	}, "-=0.5");
+
+	tl.from($('.discover').find('.text-right'), 1, {
+		autoAlpha: 0
+	});
+
+	// tl.progress(1);
+
+}
+
+
+$(document).ready(function(){
+	animation();
+});
