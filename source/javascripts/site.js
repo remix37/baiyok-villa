a4b0edbcfb4881339ee70dda622332161c461c51
Rigault Rémy <r.rigault@me.com>
2018-10-15 23:38:41 +0200

    init modal booking

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -28,7 +28,7 @@ $(window).scroll( function(){
 				var bottom_of_window = $(window).scrollTop() + $(window).height();
 				/* If the object is completely visible in the window, fade it it */
 				if( bottom_of_window > bottom_of_object ){
-					$(this).addClass("show");
+					$(this).addClass("showImages");
 				}
 		});
 });
