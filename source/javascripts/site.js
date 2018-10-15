fd8ed5069ec5d18ce557e1d8192cfc797aa07da6
Rigault Rémy <r.rigault@me.com>
2018-10-15 12:43:01 +0200

    finalisation of animation in landing page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -18,6 +18,20 @@ $(window).scroll(function() {
 		});
 }).scroll();
 
+/* Every time the window is scrolled ... */
+$(window).scroll( function(){
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+				var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+				var bottom_of_window = $(window).scrollTop() + $(window).height();
+				/* If the object is completely visible in the window, fade it it */
+				if( bottom_of_window > bottom_of_object ){
+					$(this).addClass("show");
+				}
+		});
+});
+
+
 // Animation TwinMax
 
 function animation(){
@@ -31,7 +45,7 @@ function animation(){
 
 	tl.from($('.nav-sticky'), 1, {
 		autoAlpha: 0
-	});
+	}, "-=0.5");
 
 	tl.from($('.villa'), 1.5, {
 		width: 0,
@@ -50,21 +64,26 @@ function animation(){
 
 	tl.staggerFrom($('.icon-white'), 1, {
 		autoAlpha: 0
-	}, 0.2, "-=0.5");
+	}, 0.2, "-=0.8");
 
 	tl.from($('.discover'), 1, {
 		transform: 'scaleY(0)',
 		ease:Power4.easeInOut
-	}, "-=0.5");
+	}, "-=0.8");
 
 	tl.from($('.discover').find('.text-right'), 1, {
 		autoAlpha: 0
 	});
 
+	tl.staggerFrom($('section:not(#Home), .footer'), 1, {
+		autoAlpha: 0,
+		display: 'none',
+		height: 0
+	}, 0.2 ,"-=0.8");
+
 	// tl.progress(1);
 
 }
-
 
 $(document).ready(function(){
 	animation();
